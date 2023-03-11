56d3008f2cf4c2da2791d9a36d8ae388af2bbe26
Adeilsoara <adeilsonaragao@hotmail.com>
2023-03-11 10:53:11 -0300

    Introdução ao conceito de objetos

--- aula10/script.js
+++ aula10/script.js
@@ -1,3 +1,13 @@
 /* Introdução a objetos em JavaScript */
 
 //Estrutura básica
+
+//let usuario = new Object() "Objeto construtor"
+//Objeto literal -> usando o formato chave:valor -> key:value
+let usuario = {
+    nome: "José",
+    idade: 18
+};
+
+document.write(usuario.idade)
+document.write(usuario.nome)
