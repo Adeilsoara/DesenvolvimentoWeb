cad88048234ab5387d5a7a1028d55bdc93f2cb36
Adeilsoara <adeilsonaragao@hotmail.com>
2023-03-12 10:18:48 -0300

    Exercício e Objeto Date

--- aula10/script.js
+++ aula10/script.js
@@ -12,9 +12,49 @@ let usuario = {
 /* document.write(usuario.idade)
 document.write(usuario.nome) */
 
+//Find the value of the price property and if it is greater than 100, discount it by 10%. If that’s not the case, discount it by 7%. Update the object with :
+
+//the new property - discount and the corresponding value (7% or 10%) and the new price.
+let product = {
+    name: "headphones",
+    price: 100,
+    discount: 0
+};
+if (product.price > 100) {
+  discount = ((product.price / 100) * 10);
+  product.price = (product.price - discount);
+} else {
+  discount = ((product.price / 100) * 7);
+  product.price = (product.price - discount);
+}
+console.log (product.price);
+console.log("Updated product:", product);
+
 /* Percorrendo um objeto - for in */
 
-for (const key in usuario) {
+/* for (const key in usuario) {
     document.write(key)
     document.write(usuario[key])
-}
+} */
+
+/* Converter objeto para vizulização em JSON */
+/* let objetoConvertido = JSON.stringify(usuario) */
+//document.write(JSON.stringify(usuario))
+
+/* Usando o objeto Date() em JavaScript */
+let data = new Date()
+document.write(data, "<br>")
+//document.write(`<br> A variável  'data' é do tipo: ${typeof(data)}`)
+document.write(data.getTime(), "<br>")
+document.write(Date.now())
+//instâncias do objeto Date
+/* let dia = data.getDate()
+document.write(dia)
+let mes = (data.getMonth()+1)
+document.write(mes)
+let ano = data.getFullYear()
+document.write(ano)
+let horas = data.getHours()
+document.write(horas)
+let minutos = data.getMinutes()
+document.write(minutos) */
