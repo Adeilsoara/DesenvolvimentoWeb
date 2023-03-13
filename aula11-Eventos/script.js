189ab17b7b489263dc9d9109c2564528ffe1944e
Adeilsoara <adeilsonaragao@hotmail.com>
2023-03-13 17:52:23 -0300

    Eventos JavaScript

---
+++ aula11-Eventos/script.js
@@ -0,0 +1,20 @@
+function ola() {
+  alert("Olha aí um evento em JS!!O onclick");
+}
+
+function over() {
+  document.write("Mouse over");
+}
+
+function out() {
+  document.write("Mouse out");
+}
+
+function carregarPagina() {
+  alert("Página Carregada!");
+}
+
+function mudarTexto() {
+  var x = document.getElementById("fname")
+  x.value  = x.value.toUpperCase()
+}
